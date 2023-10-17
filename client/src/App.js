f100e72e013483bebc905a620494a82adb486048
ranjan_4590 <ranjangupta35558@gmail.com>
2023-10-17 12:18:40 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Signin from './component/Signin';
+
+function App() {
+  return (
+    <div >
+      <Signin/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // screens: {
+      //   'sm': '640px',    // Small screens (e.g., smartphones)
+      //   'md': '768px',    // Medium screens (e.g., tablets)
+      //   'lg': '1024px',   // Large screens (e.g., laptops)
+      //   'xl': '1280px',   // Extra large screens (e.g., larger desktop monitors)
+      //   'custom-lg': '1100px',  
+      // },
+    },
+  },
+  plugins: [],
+}
+
